551a4e45c46163d14ad93ec7ef94dcc0403bc7b6
SongWooJeong <th171717@hanmail.net>
2021-06-20 10:23:42 +0900

    mini

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// 여러개의 프로미스 연결 가능 => 1번째 .then결과 값을 다음 .then로 넘겨주게 된다.
+
 function loadItems() {
     // fetch에 경로를 지정하여 데이터 파일을 받아욘다.
     return fetch('data/data.json')
@@ -6,6 +8,7 @@ function loadItems() {
       // json 안이 있는 데이터를 반환
       .then(json => json.items);
   }
+
 
 function displayItems(items) {
        const container = document.querySelector('.items');
@@ -43,8 +46,15 @@ function setEventListeners(items) {
     buttons.addEventListener('click', event => onButtonClick(event, items));
 }
 
+// 비동기 처리? '특정 코드의 실행이 완료될 때까지 기다리지 않고 다음코드를 먼저 수행하는 자바스크립트의 특성'
+// 프로미스? 비동기 처리에 사용되는 객체, 주로 서버에서 받아온 데이터를 화면에 표시할 때 사용
+// 프로미스의 이행(완료)상태가 되면 .then을 이용하여 처리 결과 값을 받을 수 있다.
+
+// 다른 방법으로 async와 await를 사용할 수도 있다.
+// => async와 await는 자바스크립트의 비동기 처리 패턴 중 가장 최근에 나온 문법
+
 // Main
-loadItems() 
+loadItems()
 // 데이터를 읽어와 아이템 전달 프로미스를 리턴
 .then(items => {
     // displayItems() 기능을 이용해 아이템들을 보여주고
