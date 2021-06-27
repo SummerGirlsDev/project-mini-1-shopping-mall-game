2f4a0f4675a5b9c350139ef51edb714b4fea590c
joji7752 <joji7752@gmail.com>
2021-06-27 10:17:21 +0900

    code update 2

--- src/main.js
+++ src/main.js
@@ -1,110 +1,48 @@
-const mainUl = document.querySelector('.product-ul')
-const filterP = document.querySelector('.filter-p')
-const filterT = document.querySelector('.filter-t')
-const filterS = document.querySelector('.filter-s')
-const filterBlue = document.querySelector('.filter-blue')
-const filterYellow = document.querySelector('.filter-yellow')
-const filterPink = document.querySelector('.filter-pink')
 
-const product = [{
-   src : 'img/pink_s.png',
-   text: 'female, small size'
-},{
-   src : 'img/blue_p.png',
-   text : 'man, small size' 
-},{
-   src : 'img/pink_t.png',
-   text: 'female, large size'
-},{
-   src : 'img/yellow_p.png',
-   text: 'female, large size'
-},{
-   src : 'img/blue_p.png',
-   text : 'man, small size' 
-},{
-   src : 'img/yellow_s.png',
-   text: 'man, small size'
-},{
-   src :'img/pink_p.png',
-   text: 'female, small size'
-},{
-   src : 'img/yellow_t.png',
-   text: 'male, large size'
-},{
-   src : 'img/blue_s.png',
-   text: 'female, small size'
-},{
-   src :'img/blue_t.png',
-   text: 'male, large size'
-},{
-   src : 'img/pink_s.png',
-   text: 'female, small size'
-},{
-   src : 'img/yellow_p.png',
-   text: 'female, large size'
-},{
-   src : 'img/yellow_s.png',
-   text: 'man, small size'
-}]
-
-function listshow(value){
-   mainUl.innerHTML += `<li class="product-list"><img src="${value.src}"><span class="product-name">${value.text}</span></li>`
+function loadItems() {
+   return fetch('data/data.json')
+   .then(response => response.json())
+   .then(json => json.items);
 }
 
-product.forEach((value) => listshow(value))
-
-function listdel(){
-   mainUl.innerHTML = '';
+// Update the list with the given items
+function displayItems(items){
+   const container = document.querySelector('.product-ul')
+   container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
-String.prototype.contain = function (data){
-   return this.indexOf(data) >=0
+function createHTMLString(item) {
+   return `
+   <li class="product-list">
+      <img src="${item.image}" alt="${item.type}"  />
+      <span class="product-name">${item.gender}, ${item.size}</span>
+   </li>
+   `;
 }
 
-filterP.addEventListener('click', (event) => {
-   listdel()
-   const test = product.filter(function(value){
-      return value.src.contain('_p')
-   })
-   test.forEach((value) => listshow(value))
-})
+function onButtonClick(event, items) {
+   const dataset = event.target.dataset;
+   const key = dataset.key;
+   const value = dataset.value;
 
-filterT.addEventListener('click', (event) => {
-   listdel()
-   const test = product.filter(function(value){
-      return value.src.contain('_t')
-   })
-   test.forEach((value) => listshow(value))
-})
+   if (key == null || value == null) {
+      return;
+   }
 
-filterS.addEventListener('click', (event) => {
-   listdel()
-   const test = product.filter(function(value){
-      return value.src.contain('_s')
-   })
-   test.forEach((value) => listshow(value))
-})
+   displayItems(items.filter(item => item[key] === value));
+}
 
-filterBlue.addEventListener('click', (event) => {
-   listdel()
-   const test = product.filter(function(value){
-      return value.src.contain('blue')
-   })
-   test.forEach((value) => listshow(value))
-})
 
-filterYellow.addEventListener('click', (event) => {
-   listdel()
-   const test = product.filter(function(value){
-      return value.src.contain('yellow')
-   })
-   test.forEach((value) => listshow(value))
-})
+function setEventListeners(items) {
+   const logo = document.querySelector('.logo');
+   const buttons = document.querySelector('.buttons');
+   logo.addEventListener('click', () => displayItems(items));
+   buttons.addEventListener('click', event => onButtonClick(event, items));
+}
 
-filterPink.addEventListener('click', (event) => {
-   listdel()
-   const test = product.filter(function(value){
-      return value.src.contain('pink')
+loadItems()
+   .then(items => {
+      displayItems(items);
+      setEventListeners(items);
    })
-   test.forEach((value) => listshow(value))
-})
+   .catch(console.log);
